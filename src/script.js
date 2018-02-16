87002ae194e594953d6efa1183cb289dea3aa7d3
Trevor Johnson <johnsontrevor55@gmail.com>
2018-02-15 21:32:27 -0700

    redid some js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,8 @@
 window.onload = function() {
-  var wordContainer = document.getElementById('word-container');
-  var correctLetters = document.getElementById('correct-letters');
-  var gameIsOver = document.getElementById('game-is-over');
-  var triesLeft = document.getElementById('tries-left');
-  
+  var wordContainerDiv = document.getElementById('word-container');
+  var correctLettersDiv = document.getElementById('correct-letters');
+  var triesLeftDiv = document.getElementById('tries-left');
+
   var game = {
     words: [ 'Link','Zelda','Ganondorf','Navi','Epona','Great Deku Tree','Saria',
       'Majoras Mask','Dark Link','Impa','Sheik','Tingle','Great Fairy','Postman','Skullkid','Hyrule',
@@ -11,10 +10,6 @@ window.onload = function() {
       'Lake Hylia','Lon Lon Ranch','Clock Town','Termina','Rupees','Master Sword','Hylian Shield','Mirror Shield','Ocarina of Time',
       'Hookshot','Light Arrows','Biggorn Sword','Triforce'
     ],
-
-    // sounds: [
-    //   {win: new Audio('../assets/sounds/OOT_Get_SmallItem2.wav')}
-    // ],
 
     wordInUse: [],
     guessedLetter: [],
@@ -41,6 +36,7 @@ window.onload = function() {
         newP.classList.add('mb-0');
         newDiv.appendChild(newP);
 
+        //reveals white space automatically
         if (newP.textContent === ' ') {
           newDiv.classList.add('border-0');
         } else {
@@ -48,16 +44,15 @@ window.onload = function() {
         }
 
         newDiv.classList.add('letter-content');
-        document.getElementById('word-container').appendChild(newDiv);
+        wordContainerDiv.appendChild(newDiv);
       }
     },
 
     //removes all child nodes and sets values back to default.
     newGame: function() {
-      document.getElementById('word-container').innerHTML = '';
-      document.getElementById('correct-letters').innerHTML = '';
-      document.getElementById('game-is-over').innerHTML = '';
-      document.getElementById('tries-left').innerHTML = `<p>Tries Left </p><p>6</p>`;
+      wordContainerDiv.innerHTML = '';
+      correctLettersDiv.innerHTML = '';
+      triesLeftDiv.innerHTML = `<p>Tries Left </p><p>6</p>`;
       this.displayWordLength();
       this.guessedLetter = [];
       this.triesLeft = 6;
@@ -65,33 +60,28 @@ window.onload = function() {
     },
 
     //Revealed letters have a class of border-0. Checks for a win by seeing if all letters have a class of border-0
-    checkWin: function() {
+    checkWinLoss: function(letter) {
+      var winLossCounterChildren = document.getElementById('win-loss-counter').childNodes;  
       var revealedLetters = document.getElementsByClassName('border-0').length;
       var totalLetters = this.wordInUse.length;
-
+      
       if (revealedLetters === totalLetters) {
-        var children = document.getElementById('win-loss-counter').childNodes;
         this.gameIsOver = true;
         game.wins++;
-        children[1].textContent = `Wins: ${game.wins}`;
-        document.getElementById('tries-left').innerHTML = 'You Win!';
+        winLossCounterChildren[1].textContent = `Wins: ${game.wins}`;
+        triesLeftDiv.innerHTML = `<p>You Win!</p>`;
         setTimeout(this.newGame.bind(this), 1000);
       }
-    },
-
-    checkLoss: function(letter) {
-      var lossTrackerChildren = document.getElementById('win-loss-counter').childNodes;
+      
       if (!game.wordInUse.includes(letter)) {
-        var triesLeftContent = document.getElementById('tries-left');
         this.triesLeft--;
-
         if (game.triesLeft > 0) {
-          triesLeftContent.innerHTML = `<p>Tries Left </p><p>${game.triesLeft}</p>`;
+          triesLeftDiv.innerHTML = `<p>Tries Left </p><p>${game.triesLeft}</p>`;
         } else {
           this.gameIsOver = true;
           game.losses++;
-          lossTrackerChildren[3].textContent = `Losses: ${game.losses}`;
-          document.getElementById('tries-left').innerHTML = 'Game Over';
+          winLossCounterChildren[3].textContent = `Losses: ${game.losses}`;
+          triesLeftDiv.innerHTML = `<p>Game Over</p>`;
           setTimeout(this.newGame.bind(this), 2000);
         }
       }
@@ -105,13 +95,13 @@ window.onload = function() {
     var letter = event.key.toUpperCase();
     var letterCode = letter.charCodeAt(0);
 
+    //only runs if a a letter is pressed
     if (!game.gameIsOver && letterCode >= 65 && letterCode <= 90 || letterCode >= 97 && letterCode <= 122) {
 
       //Reveals correct letter
       game.wordInUse.map((a,index) => {
         if (a === letter) {
-          var parent = document.getElementById('word-container');
-          var children = parent.childNodes;      
+          var children = wordContainerDiv.childNodes;      
           var getP = children[index].firstChild;
           children[index].classList.add('border-0');
           getP.classList.remove('d-none');
@@ -121,14 +111,13 @@ window.onload = function() {
             var newDiv = document.createElement('div');
             newDiv.classList.add('correct-guess');
             newDiv.textContent = a;
-            document.getElementById('correct-letters').appendChild(newDiv);
+            correctLettersDiv.appendChild(newDiv);
             game.guessedLetter.push(a);
           }
         }
       });
 
-      game.checkWin();
-      game.checkLoss(letter)
+      game.checkWinLoss(letter);
     }
   }
 }
